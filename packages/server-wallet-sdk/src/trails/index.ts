0a21067f57b8c30b62947b421b1e9ae590e42cc0
Taylan Pince <taylanpince@gmail.com>
2026-09-17 18:55:10 +0200

    Add Trails SDK foundation and verified typed-data signing

---
+++ packages/server-wallet-sdk/src/trails/index.ts
@@ -0,0 +1,6 @@
+export { TrailsClient, TrailsError } from './client.js';
+export type { TrailsClientOptions } from './client.js';
+export { buildSwapRequest, validateSwapQuote, tokenAddress } from './quote.js';
+export type { SwapRequest, SwapAsset, SwapQuote, SwapQuoteRequest } from './quote.js';
+export { validateRecoveryPayload } from './recovery.js';
+export type { TrailsIntent, TrailsToken, TrailsContracts, PreparedRecovery } from './protocol.js';
